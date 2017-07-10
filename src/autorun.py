f269270455393df1860b6990c81534a0d471a154
Shlw <shlw_kevin@hotmail.com>
2017-07-10 10:52:12 +0800

    update autorun

--- src/autorun.py
+++ src/autorun.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 path = '../data/uuf50-218/UUF50.218.1000/'
-path = '../data/uf20-91/'
+#path = '../data/uf20-91/'
 os.system('ls %s > list.txt' % path)
 f = open('list.txt', 'r')
 for x in f.readlines():
